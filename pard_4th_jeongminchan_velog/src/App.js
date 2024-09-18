f3c98357be807c6706d62d467d5aa48735d54831
jeongminchan <104063175+MinchoLover@users.noreply.github.com>
2024-09-19 00:22:39 +0900

    React App - Velog

---
+++ pard_4th_jeongminchan_velog/src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import {
+    BaseContainer,
+    Body,
+    Bottom,
+    Box,
+    Button,
+    Checkbox,
+    Email,
+    H1,
+    Header,
+    Input,
+    Introduce,
+    Label,
+    Name,
+    Page,
+    Password,
+    Span
+} from './RegisterPage/Mycomponent';
+
+function App() {
+    return (
+        <Page>
+            <BaseContainer>
+
+                <Header>
+                    <H1>환영합니다!</H1>
+                    <p>기본 회원 정보를 등록해주세요</p>
+                </Header>
+
+                <Body>
+                    <Box>
+                        <Label>이름</Label>
+                        <Name>
+                            <Input type="text" placeholder='이름을 입력해주세요.' />
+                        </Name>
+                    </Box>
+
+                    <Box>
+                        <Label>이메일</Label>
+                        <Email>
+                            <Input type="text" placeholder='이메일을 입력해주세요.' />
+                        </Email>
+                    </Box>
+
+                    <Box>
+                        <Label>비밀번호</Label>
+                        <Password>
+                            <Input type="password" placeholder='비밀번호를 입력해주세요.' />
+                        </Password>
+                    </Box>
+
+                    <Box>
+                        <Label>한 줄 소개</Label>
+                        <Introduce>
+                            <Input type="text" placeholder='당신을 한 줄로 소개해보세요' />
+                        </Introduce>
+                    </Box>
+
+                    <Label>
+                        <Checkbox type="checkbox" />
+                        <Span>이용약관</Span>과 <Span>개인정보취급방침</Span>에 동의합니다.
+                    </Label>
+
+                </Body>
+
+                <Bottom>
+                    <Button bgColor="orange">취소</Button>
+                    <Button>가입</Button>
+                </Bottom>
+
+            </BaseContainer>
+        </Page>
+    );
+}
+
+export default App;
---
+++ pard_4th_jeongminchan_velog/src/RegisterPage/RegisterPage.js
@@ -0,0 +1,58 @@
+import './RegisterPage.css';
+
+function RegisterPage() {
+    return (  
+        <div className="Page">
+            <div className="BaseContainer">
+                {/* 헤더 요소 */}
+                <div className="Header">
+                    <h1>환영합니다!</h1>
+                    <p>기본 회원 정보를 등록해주세요</p>
+                </div>
+                {/* 메인 요소 */}
+                <div className="Body">
+                    <div className="Box">
+                        <p className="Label">이름</p>
+                        <div className="Name">
+                            <input type="text" placeholder="이름을 입력해주세요." />
+                        </div>
+                    </div>
+
+                    <div className="Box">
+                        <p className="Label">이메일</p>
+                        <div className="Email">
+                            <input type="text" placeholder="이메일을 입력해주세요." />
+                        </div>
+                    </div>
+
+                    <div className="Box">
+                        <p className="Label">비밀번호</p>
+                        <div className="Password">
+                            <input type="password" placeholder="비밀번호를 입력해주세요." />
+                        </div>
+                    </div>
+
+                    <div className="Box">
+                        <p className="Label">한 줄 소개</p>
+                        <div className="Introduce">
+                            <input type="text" placeholder="당신을 한 줄로 소개해보세요" />
+                        </div>
+                    </div>
+                    {/* 체크박스 요소 */}
+                    <p className="Label">
+                        <input type="checkbox" />
+                        <span className="Span">이용약관</span>과 <span className="Span">개인정보취급방침</span>에 동의합니다.
+                    </p>
+                </div>
+                {/* 푸터 요소 */}
+                <div className="Bottom">
+                    <button className="Button">취소</button>
+                    <button className="Button">가입</button>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default RegisterPage;
